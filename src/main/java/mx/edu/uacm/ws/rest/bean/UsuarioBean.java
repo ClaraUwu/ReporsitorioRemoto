5905110d51106fe27569251128972d7381ed0ceb
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 17:21:16 -0600

    Generación de los Constructores (vacío y parametrizado) y de los Setters y Getters que contiene: id: Identificador único (clave primaria, autogenerado). email: Correo del usuario (único y obligatorio). password: Contraseña del usuario.

--- src/main/java/mx/edu/uacm/ws/rest/bean/UsuarioBean.java
+++ src/main/java/mx/edu/uacm/ws/rest/bean/UsuarioBean.java
@@ -18,6 +18,42 @@ public class UsuarioBean {
 	private String email;
 	@Column(nullable = false)
 	private String password;
+	
+	public UsuarioBean() {
+	}
+	
+	public UsuarioBean(Long id, String email, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
 
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+	
 }
