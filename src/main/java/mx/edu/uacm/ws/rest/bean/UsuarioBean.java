b87f410ba2b10f990d930c99fba15011a534e335
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 17:18:34 -0600

    Creación de la clase UsuarioBean que será la Entidad Usuario.

---
+++ src/main/java/mx/edu/uacm/ws/rest/bean/UsuarioBean.java
@@ -0,0 +1,23 @@
+package mx.edu.uacm.ws.rest.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+public class UsuarioBean {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+
+
+}
