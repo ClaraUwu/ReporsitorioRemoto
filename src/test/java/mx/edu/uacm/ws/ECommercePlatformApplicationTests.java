c17b4882eb3af3cbcd91fa8a1ebf47d7037bb06e
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 15:37:44 -0600

    Inicialización del proyecto

---
+++ src/test/java/mx/edu/uacm/ws/ECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uacm.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
