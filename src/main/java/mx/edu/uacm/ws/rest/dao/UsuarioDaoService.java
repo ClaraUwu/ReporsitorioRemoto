bc80d719763701913be4bab2452f2a181ce3a342
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 20:02:44 -0600

    Registrará un usuario: registrarUsuario(UsuarioBean usuario). Buscará un usuario por email: buscarPorEmail(String email).

--- src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
+++ src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
@@ -1,5 +1,23 @@
 package mx.edu.uacm.ws.rest.dao;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import mx.edu.uacm.ws.rest.bean.UsuarioBean;
+import mx.edu.uacm.ws.rest.repository.UsuarioRepository;
+
+@Component
 public class UsuarioDaoService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public UsuarioBean registrarUsuario(UsuarioBean usuario) {
+		return usuarioRepository.save(usuario);	
+	}
+	
+	public Optional<UsuarioBean> buscarPorEmail(String email){
+		return usuarioRepository.findByEmail(email);
+	}
 
 }
