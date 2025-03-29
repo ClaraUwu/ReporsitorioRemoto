f1a10e46fcf9163bac8bc0b46cfa6963058fbf19
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 18:30:11 -0600

    Extensión de JpaRepository<UsuarioBean, Long> para proporcionar operaciones CRUD en la entidad UsuarioBean. Método adicional: Optional<UsuarioBean> findByEmail(String email) → Permitirá buscar usuarios por email.

--- src/main/java/mx/edu/uacm/ws/rest/repository/UsuarioRepository.java
+++ src/main/java/mx/edu/uacm/ws/rest/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package mx.edu.uacm.ws.rest.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import mx.edu.uacm.ws.rest.bean.UsuarioBean;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioBean, Long>{
+	
+	Optional<UsuarioBean> findByEmail(String email);
 
 }
