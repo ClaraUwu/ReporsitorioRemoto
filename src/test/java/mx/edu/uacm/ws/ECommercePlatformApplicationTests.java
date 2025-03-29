068356dc4dfc232b246721f56a8a5f2a75cc6e94
patat <patat@DESKTOP-FOMQKA8>
2025-03-28 20:29:46 -0600

    Se usará JUnit y Mockito para probar la lógica del servicio UsuarioDaoService. Prueba: registrarUsuario() → Simula el registro de un usuario y verifica que el email se guarda correctamente.

--- src/test/java/mx/edu/uacm/ws/ECommercePlatformApplicationTests.java
+++ src/test/java/mx/edu/uacm/ws/ECommercePlatformApplicationTests.java
@@ -1,13 +1,32 @@
 package mx.edu.uacm.ws;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import mx.edu.uacm.ws.rest.bean.UsuarioBean;
+import mx.edu.uacm.ws.rest.dao.UsuarioDaoService;
+import mx.edu.uacm.ws.rest.repository.UsuarioRepository;
 
 @SpringBootTest
 class ECommercePlatformApplicationTests {
 
+	@Mock
+	private UsuarioRepository usuarioRepository;
+	
+	@InjectMocks
+	private UsuarioDaoService service;
+	
 	@Test
-	void contextLoads() {
+	void registrarUsuario() {
+		UsuarioBean usuarioBean = new UsuarioBean(null, "test@email.com", "password123");
+		when(usuarioRepository.save(any())).thenReturn(usuarioBean);
+		UsuarioBean resultado = service.registrarUsuario(usuarioBean);
+		assertEquals("test@email.com", resultado.getEmail());
+		
 	}
 
 }
