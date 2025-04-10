c249560c73b0031be7a57b94898faa3dbe80921a
patat <patat@DESKTOP-FOMQKA8>
2025-04-09 20:47:29 -0600

    Validaciones y Javadoc

--- src/main/java/mx/edu/uacm/ws/rest/bean/UsuarioBean.java
+++ src/main/java/mx/edu/uacm/ws/rest/bean/UsuarioBean.java
@@ -6,7 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+/**
+ * Entidad que representa a un usuario en la plataforma de e-commerce.
+ */
 @Entity
 @Table(name="usuarios")
 public class UsuarioBean {
@@ -14,8 +20,14 @@ public class UsuarioBean {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+    @Email(message = "El correo electronico debe ser valido")
+    @NotBlank(message = "El correo electrónico no puede estar vacio")
 	@Column(unique = true, nullable = false)
 	private String email;
+    
+    @NotBlank(message = "La contrasena no puede estar vacia")
+    @Size(min = 8, message = "La contrasena debe tener al menos 8 caracteres")
 	@Column(nullable = false)
 	private String password;
 	
