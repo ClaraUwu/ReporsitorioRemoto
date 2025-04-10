b018913b4379ee4e55d1de360442020c8bdc5137
patat <patat@DESKTOP-FOMQKA8>
2025-04-09 21:16:37 -0600

    Estandarizacion de UsuarioController

--- src/main/java/mx/edu/uacm/ws/rest/controller/UsuarioController.java
+++ src/main/java/mx/edu/uacm/ws/rest/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package mx.edu.uacm.ws.rest.controller;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import mx.edu.uacm.ws.rest.bean.UsuarioBean;
 import mx.edu.uacm.ws.rest.dao.UsuarioDaoService;
 
