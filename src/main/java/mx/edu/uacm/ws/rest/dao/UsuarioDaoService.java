b2bc3a675ad60e4d77427e40981f9b4a7e2f8ff8
patat <patat@DESKTOP-FOMQKA8>
2025-04-09 21:17:35 -0600

    Estandarizacion de UsuarioDaoService

--- src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
+++ src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
@@ -1,13 +1,15 @@
 package mx.edu.uacm.ws.rest.dao;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import mx.edu.uacm.ws.rest.bean.UsuarioBean;
 import mx.edu.uacm.ws.rest.repository.UsuarioRepository;
 
 /**
- * Servicio DAO para la gestión de usuarios.
+ * Servicio DAO para la gestion de usuarios.
  */
 @Component
 public class UsuarioDaoService {
