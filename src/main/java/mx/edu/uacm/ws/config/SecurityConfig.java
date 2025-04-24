dc185795eccddfe7464ca791b5f24edd82bdde2c
patat <patat@172.20.206.227>
2025-04-24 09:25:14 -0600

    Seguridad con Spring Security

--- src/main/java/mx/edu/uacm/ws/config/SecurityConfig.java
+++ src/main/java/mx/edu/uacm/ws/config/SecurityConfig.java
@@ -1,5 +1,32 @@
 package mx.edu.uacm.ws.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.csrf().disable()
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/api/usuarios/registro").permitAll()
+					.anyRequest().authenticated()
+			)
+			.httpBasic();
+		
+		return http.build();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+		
+	}
 
 }
