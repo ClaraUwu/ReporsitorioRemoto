32b6e34d6373693019be02962a3d1c4afbc4a6f7
patat <patat@172.20.206.227>
2025-04-24 09:07:39 -0600

    Cifrado y excepciones específicas en UsuarioDaoService

--- src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
+++ src/main/java/mx/edu/uacm/ws/rest/dao/UsuarioDaoService.java
@@ -3,6 +3,7 @@ package mx.edu.uacm.ws.rest.dao;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import mx.edu.uacm.ws.rest.bean.UsuarioBean;
@@ -17,15 +18,19 @@ public class UsuarioDaoService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public UsuarioBean registrarUsuario(UsuarioBean usuario) {
 		 if (usuario == null || usuario.getEmail() == null || !usuario.getEmail().contains("@")) {
 	            throw new IllegalArgumentException("Email invalido");
 	        }
 
-	        if (usuario.getPassword() == null || usuario.getPassword().length() < 8) {
-	            throw new IllegalArgumentException("La contrasena debe tener al menos 8 caracteres");
-	        }
-	        
+        if (usuario.getPassword() == null || usuario.getPassword().length() < 8) {
+            throw new IllegalArgumentException("La contrasena debe tener al menos 8 caracteres");
+        }
+	    
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 		return usuarioRepository.save(usuario);	
 	}
 	
