068cb8e570174ca4d9878575dfb2a90235fab34f
patat <patat@DESKTOP-FOMQKA8>
2025-04-25 21:16:17 -0600

    Declaración de  serialVersionUID

--- src/main/java/mx/edu/uacm/ws/exception/ValidacionException.java
+++ src/main/java/mx/edu/uacm/ws/exception/ValidacionException.java
@@ -1,7 +1,8 @@
 package mx.edu.uacm.ws.exception;
 
 public class ValidacionException extends RuntimeException{
-	
+	private static final long serialVersionUID = 1L;
+
 	public ValidacionException(String mensaje) {
 		super(mensaje);
 	}
